3a4b20a0116d44743a2534606371863e7d766f85
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-11-07 16:24:57 -0500

    🔴 Si_AgregoUnProducto_Debe_RetornarElValorTotal

--- SuperMarket/ReciboDeSupermercado.cs
+++ SuperMarket/ReciboDeSupermercado.cs
@@ -5,9 +5,26 @@ namespace SuperMarket;
 public class ReciboDeSupermercado
 {
     [Fact]
-    public void x()
+    public void Si_AgregoUnProducto_Debe_RetornarElValorTotal()
     {
-      
-    }
+        //arrange
+        var productos = new List<(string nombre, decimal precio, decimal cantidad)>
+        {
+            ("Arroz", 2.49m, 1),
+        };
 
+        //act
+        var total = Carrito.Calcular(productos);
+        
+        //Asert
+        total.Should().Be("2.49m");
+    }
+}
+
+public class Carrito
+{
+    public static object Calcular(object productos)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ SuperMarket/TestList.md
@@ -0,0 +1,7 @@
+
+# 🧾 Test List - Kata Supermercado
+
+## Primera parte: Cálculo básico
+- [ ] Valor de 1 producto → total unidad 
+- [ ] valor de 2 productos → total (Suma Correcta)
+- [ ] Calcular total con varios productos → total (Suma correcta)
