2c04ffb884fbfc310a4c3fbb32d8a05d4b4555d6
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-11-07 17:06:12 -0500

    ✅ Si_AgregoUnProductoCon2Unidades_Debe_RetornarElValorTotal

--- SuperMarket/ReciboDeSupermercado.cs
+++ SuperMarket/ReciboDeSupermercado.cs
@@ -59,7 +59,7 @@ public class Carrito
 {
     public static decimal Calcular(List<(string nombre, decimal precio, decimal unidad)> productos)
     {
-        return productos.Sum(p => p.precio);
+        return productos.Sum(p => p.precio * p.unidad);
 
     }
 }
