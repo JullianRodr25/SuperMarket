a6a9849110c0e12b1fb470bf9bb690ebb41ce56a
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-11-10 11:06:13 -0500

    ✅ Si_Agrego3UnidadesDeCepillosDeDientes_Debe_RetornarELValorTotalAplicandoElDescuento

--- SuperMarket/Carrito.cs
+++ SuperMarket/Carrito.cs
@@ -4,7 +4,20 @@ public class Carrito
 {
     public static decimal Calcular(List<(string nombre, decimal precio, decimal unidad)> productos)
     {
-        return productos.Sum(p => p.precio * p.unidad);
+        var subtotal = productos.Sum(p => p.precio * p.unidad);
+        var descuentos = AplicaDescuentos(productos);
+        return subtotal - descuentos;
+    }
 
+    private static decimal AplicaDescuentos(List<(string nombre, decimal precio, decimal unidad)> productos)
+    {
+        var cepillos = productos.FirstOrDefault(p => p.nombre == "Cepillo de dientes");
+
+        if (cepillos.nombre != null && cepillos.unidad >= 3)
+        {
+            return 0.99m;
+        }
+
+        return 0m;
     }
 }
