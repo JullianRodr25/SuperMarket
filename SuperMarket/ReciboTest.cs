0a6233ac50e95026ba4feda6f8fdf1fd55d3cde2
Julian Rodriguez Vargas <Julian.vargas@sinco.com.co>
2025-11-10 23:01:41 -0500

    🔴 Si_ComproProductosConDescuentosOPromociones_Debe_GenerarReciboConDetallesDeLaCompraTotalYTotalDescuentos

--- SuperMarket/ReciboTest.cs
+++ SuperMarket/ReciboTest.cs
@@ -17,16 +17,17 @@ public class ReciboTest
 
         // Act
         var recibo = carrito.GenerarRecibo(productos);
-    
+
         //Assert
         recibo.ProductosDetalles.Should().ContainSingle();
         recibo.ProductosDetalles[0].Should().Contain("Arroz");
         recibo.Total.Should().Be(2.49m);
         recibo.TotalDescuentos.Should().Be(0m);
     }
-    
+
     [Fact]
-    public void Si_Compro1BolsaDeArrozY1DeLeche_Debe_GenerarReciboConDosProductosSinDescuentoYDebeIncluirAmbosEnElDetalle()
+    public void
+        Si_Compro1BolsaDeArrozY1DeLeche_Debe_GenerarReciboConDosProductosSinDescuentoYDebeIncluirAmbosEnElDetalle()
     {
         // Arrange
         var productos = new List<(string nombre, decimal precio, decimal unidad)>
@@ -38,7 +39,7 @@ public class ReciboTest
 
         // Act
         var recibo = carrito.GenerarRecibo(productos);
-    
+
         // Assert
         recibo.ProductosDetalles.Should().HaveCount(2);
         recibo.ProductosDetalles[0].Should().Contain("Arroz");
@@ -46,7 +47,7 @@ public class ReciboTest
         recibo.Total.Should().Be(4.48m);
         recibo.TotalDescuentos.Should().Be(0m);
     }
-    
+
     [Fact]
     public void Si_ComproUnProducto_Debe_GenerarReciboYDebeIncluirNombrePrecioYCantidad()
     {
@@ -59,12 +60,43 @@ public class ReciboTest
 
         // Act
         var recibo = carrito.GenerarRecibo(productos);
-    
+
         // Assert
         recibo.ProductosDetalles[0].Should().Contain("Arroz");
         recibo.ProductosDetalles[0].Should().Contain("2,49");
         recibo.ProductosDetalles[0].Should().Contain("2");
         recibo.Total.Should().Be(4.98m);
     }
-    
+
+    [Fact]
+    public void
+        Si_ComproProductosConDescuentosOPromociones_Debe_GenerarReciboConDetallesDeLaCompraTotalYTotalDescuentos()
+    {
+        // Arrange
+        var productos = new List<(string nombre, decimal precio, decimal unidad)>
+        {
+            ("Cepillo de dientes", 0.99m, 3),
+            ("Arroz", 2.49m, 2),
+        };
+        var carrito = new Carrito();
+        carrito.AplicarDescuentosSemana();
+
+        // Act
+        var recibo = carrito.GenerarRecibo(productos);
+
+        // Assert
+        recibo.ProductosDetalles.Should().HaveCount(2);
+        // 3 cepillos, pagas 2
+        recibo.ProductosDetalles[0].Should().Contain("Cepillo de dientes");
+        recibo.ProductosDetalles[0].Should().Contain("0,99");
+        recibo.ProductosDetalles[0].Should().Contain("3");
+        // 10% de descuento en arroz
+        recibo.ProductosDetalles[1].Should().Contain("Arroz");
+        recibo.ProductosDetalles[1].Should().Contain("2,49");
+        recibo.ProductosDetalles[1].Should().Contain("2");
+
+        // totales
+        recibo.Total.Should().Be(6.46m);
+        recibo.TotalDescuentos.Should().Be(1.49m);
+    }
 }
--- SuperMarket/obj/Debug/net9.0/SuperMarket.AssemblyInfo.cs
+++ SuperMarket/obj/Debug/net9.0/SuperMarket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a875ae62bcfd573cc90af63174cf803cbd77ae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351033a27c088d1115017e79ae1827a9e8a2e91b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
